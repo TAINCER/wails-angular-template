b2d351486b8d4e202f4d7d1cb9ab94f9b5457797
Timm Ortloff <me@tortloff.de>
2021-10-22 05:28:53 +0200

    Added about route

--- frontend/src/app/about/about-routing.module.ts
+++ frontend/src/app/about/about-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
   }
 ];
 
